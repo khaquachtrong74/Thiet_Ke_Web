e1243df537f52a390ce679e37647dd32ee467c46
DetKha <khaquachtrong74@gmail.com>
2024-08-19 20:20:16 +0700

    du an lon

--- main_js/duanlon_js.js
+++ main_js/duanlon_js.js
@@ -2,12 +2,52 @@ window.onload = function () {
     let search = document.getElementsByTagName('input')[0];
     let btn = document.getElementsByClassName('btn')[0];
     let k = document.getElementsByClassName('content')[0]
-    btn.onclick = function () {
-        // Tạo một phần tử <p> mới
+    // btn.onclick = function () {
+    //     // Tạo một phần tử <p> mới
+    //     let p = document.createElement('p');
+    //     p.textContent = search.value; // Đặt nội dung cho phần tử <p>
+    //     // alert(search.value)
+    //     let ps = document.querySelectorAll('.content>p');
+    //     let exists = false;
+    //     for (let i of ps) {
+    //         if (i.textContent === p.textContent) {
+    //             exists = true;
+    //             break;
+    //         }
+    //     }
+    //     if (exists === false) {
+    //         k.appendChild(p);
+    //     }
+    //     p.onclick = function () {
+
+    //         search.value = this.textContent
+    //     }
+    // }
+    function Compl_text() {
         let p = document.createElement('p');
         p.textContent = search.value; // Đặt nội dung cho phần tử <p>
         // alert(search.value)
-        k.appendChild(p);
+        let ps = document.querySelectorAll('.content>p');
+        let exists = false;
+        for (let i of ps) {
+            if (i.textContent === p.textContent) {
+                exists = true;
+                break;
+            }
+        }
+        if (exists === false) {
+            k.appendChild(p);
+        }
+        p.onclick = function () {
 
+            search.value = this.textContent
+        }
     }
+    btn.onclick = Compl_text;
+
+    search.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            Compl_text();
+        }
+    });
 }
